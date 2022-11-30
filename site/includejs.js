caecc9bd26e2c4fb1c35b82ecfff888399c96fc4
GZod01 <95311062+GZod01@users.noreply.github.com>
2022-11-30 07:56:19 +0100

    Update includejs.js

--- site/includejs.js
+++ site/includejs.js
@@ -6,6 +6,12 @@ function includeHTML() {
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("include-js-html");
     if (file) {
+      if (file ==='header'){
+        file = 'https://raw.githubusercontent.com/GZod01/gzod01.github.io/main/site/header.html'
+      }
+      else if (file === 'footer'){
+        file = 'https://raw.githubusercontent.com/GZod01/gzod01.github.io/main/site/footer.html'
+      }
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
