00ce0aacd6932a02eadd25e8fd2f9ea2df47da05
GZod01 <95311062+GZod01@users.noreply.github.com>
2022-11-30 14:58:56 +0100

    ok

--- site/datasave.php
+++ site/datasave.php
@@ -1,7 +1,7 @@
 if(!empty($_POST['data'])){
 $data = $_POST['data'];
 $name = $_POST['name']
-$fname = "$name.html";//generates random name
+$fname = "$name.html";
 
 $file = fopen("upload/" .$fname, 'w');
 fwrite($file, $data);
